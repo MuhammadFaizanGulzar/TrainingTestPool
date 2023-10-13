16d43298e0d9c58a58b8b64b56e15cd9b37c43a1
Faizan Gulzar <faizan.gulzar@codeninjaconsulting.com>
2023-10-13 14:23:40 +0500

    Review changes.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorOperations
+{
+    public class CalculatorOperation
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            //for (int i = 0; i < Math.Abs(num2); i++)
+            //{
+            //    result += num1;
+            //}
+
+            //return (num2 < 0) ? -result : result;
+
+            int sign = 1;
+
+            if (num1 < 0)
+            {
+                num1 = -num1;
+                sign = -sign;
+            }
+
+            if (num2 < 0)
+            {
+                num2 = -num2;
+                sign = -sign;
+            }
+
+            double result = 0;
+
+            for (int i = 0; i < num2; i++)
+            {
+                result += num1;
+            }
+
+            return sign * result;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+
+            double result = 0;
+            double absNum1 = Math.Abs(num1);
+            double absNum2 = Math.Abs(num2);
+
+            while (absNum1 >= absNum2)
+            {
+                absNum1 -= absNum2;
+                result++;
+            }
+
+            if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0))
+            {
+                result = -result;
+            }
+
+            return result;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,82 +1,33 @@
-﻿using System;
-
-class Calculator
-{
-    public double Add(double num1, double num2)
-    {
-        return num1 + num2;
-    }
-
-    public double Subtract(double num1, double num2)
-    {
-        return num1 - num2;
-    }
-
-    public double Multiply(double num1, double num2)
-    {
-        double result = 0;
-
-        for (int i = 0; i < Math.Abs(num2); i++)
-        {
-            result += num1;
-        }
-
-
-
-
-        return (num2 < 0) ? -result : result;
-    }
-
-    public double Divide(double num1, double num2)
-    {
-        if (num2 == 0)
-        {
-            throw new DivideByZeroException("Division by zero is not allowed.");
-        }
-
-        double result = 0;
-        double absNum1 = Math.Abs(num1);
-        double absNum2 = Math.Abs(num2);
-
-        while (absNum1 >= absNum2)
-        {
-            absNum1 -= absNum2;
-            result++;
-        }
-
-        if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0))
-        {
-            result = -result;
-        }
-
-        return result;
-    }
-}
+﻿using Calculator.Enum;
+using CalculatorOperations;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Calculator calculator = new Calculator();
+        CalculatorOperation calculator = new CalculatorOperation();
         bool exit = false;
 
         while (!exit)
         {
             Console.WriteLine("Select an operation:");
-            Console.WriteLine("1. Add");
-            Console.WriteLine("2. Subtract");
-            Console.WriteLine("3. Multiply");
-            Console.WriteLine("4. Divide");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine($"{(int)CalculatorOperationvalues.Add}. Add");
+            Console.WriteLine($"{(int)CalculatorOperationvalues.Subtract}. Subtract");
+            Console.WriteLine($"{(int)CalculatorOperationvalues.Multiply}. Multiply");
+            Console.WriteLine($"{(int)CalculatorOperationvalues.Divide}. Divide");
+            Console.WriteLine($"{(int)CalculatorOperationvalues.Exit}. Exit");
 
             int choice;
-            if (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 5)
+            if (!int.TryParse(Console.ReadLine(), out choice) || !Enum.IsDefined(typeof(CalculatorOperationvalues), choice))
             {
                 Console.WriteLine("Invalid input. Please enter a valid option (1-5).");
                 continue;
             }
 
-            if (choice == 5)
+            CalculatorOperationvalues operation = (CalculatorOperationvalues)choice;
+
+            if (operation == CalculatorOperationvalues.Exit)
             {
                 exit = true;
                 continue;
@@ -86,7 +37,7 @@ class Program
             double num1;
             if (!double.TryParse(Console.ReadLine(), out num1))
             {
-                Console.WriteLine("Invalid input for number. Please enter a valid number.");
+                Console.WriteLine("Invalid input for the number. Please enter a valid number.");
                 continue;
             }
 
@@ -94,35 +45,37 @@ class Program
             double num2;
             if (!double.TryParse(Console.ReadLine(), out num2))
             {
-                Console.WriteLine("Invalid input for number. Please enter a valid number.");
+                Console.WriteLine("Invalid input for the number. Please enter a valid number.");
                 continue;
             }
 
-            try
-            {
-                double result = 0;
-                switch (choice)
-                {
-                    case 1:
-                        result = calculator.Add(num1, num2);
-                        break;
-                    case 2:
-                        result = calculator.Subtract(num1, num2);
-                        break;
-                    case 3:
-                        result = calculator.Multiply(num1, num2);
-                        break;
-                    case 4:
-                        result = calculator.Divide(num1, num2);
-                        break;
-                }
+            double result = 0;
 
-                Console.WriteLine($"Result: {result}");
-            }
-            catch (DivideByZeroException ex)
+            switch (operation)
             {
-                Console.WriteLine(ex.Message);
+                case CalculatorOperationvalues.Add:
+                    result = calculator.Add(num1, num2);
+                    break;
+                case CalculatorOperationvalues.Subtract:
+                    result = calculator.Subtract(num1, num2);
+                    break;
+                case CalculatorOperationvalues.Multiply:
+                    result = calculator.Multiply(num1, num2);
+                    break;
+                case CalculatorOperationvalues.Divide:
+                    try
+                    {
+                        result = calculator.Divide(num1, num2);
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        continue;
+                    }
+                    break;
             }
+
+            Console.WriteLine($"Result: {result}");
         }
     }
 }
