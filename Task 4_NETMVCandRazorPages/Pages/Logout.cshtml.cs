289afa3678a42d9647879cbaaf38590baef13e1a
Faizan Gulzar <faizan.gulzar@codeninjaconsulting.com>
2023-10-19 16:50:51 +0500

    logout

---
+++ Task 4_NETMVCandRazorPages/Pages/Logout.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task_4_NETMVCandRazorPages.Pages
+{
+    public class LogoutModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+}
