d70535fd42a39ce736fa3531fc3931b8d318dc13
Faizan Gulzar <faizan.gulzar@codeninjaconsulting.com>
2023-11-15 15:51:05 +0500

    Initial Push.

---
+++ Task7_Razor_AWS_SQS_SNS/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task7_Razor_AWS_SQS_SNS.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
