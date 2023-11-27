3026c811c48caff6a240e6b92021c1679c46df23
Faizan Gulzar <faizan.gulzar@codeninjaconsulting.com>
2023-11-27 15:30:25 +0500

    ìnitial setup.

---
+++ Task9_AzureApplication/Pages/FileUpload.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Task9_AzureApplication.Pages
+{
+    public class FileUploadModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
