ef7e9f263f0a8679998692d6e869e31564de6615
Faizan Gulzar <faizan.gulzar@codeninjaconsulting.com>
2023-12-04 11:45:12 +0500

    optimizations.

--- Task9_AzureApplication/Helper/NotificationHub.cs
+++ Task9_AzureApplication/Helper/NotificationHub.cs
@@ -11,17 +11,15 @@ namespace Task9_AzureApplication.Helper
 
         public override async Task OnConnectedAsync()
         {
-            // This method is called when a connection with the hub is established.
+            //A connection with the hub is established.
 
-            // You can use it to track connections or perform other tasks.
             await base.OnConnectedAsync();
         }
 
         public override async Task OnDisconnectedAsync(Exception exception)
         {
-            // This method is called when a connection is terminated.
+            // When a connection is terminated.
 
-            // You can use it to track disconnections or perform other cleanup tasks.
             await base.OnDisconnectedAsync(exception);
         }
     }
